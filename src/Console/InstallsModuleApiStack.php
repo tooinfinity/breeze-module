bcaab2f5633db3105d738dbd85b5a0d10bb73504
tooinfinity <touwfiqdev@gmail.com>
2024-08-10 11:26:12 +0100

    add command install:api for module api

--- src/Console/InstallsModuleApiStack.php
+++ src/Console/InstallsModuleApiStack.php
@@ -13,7 +13,7 @@ trait InstallsModuleApiStack
      */
     protected function installModuleApiStack()
     {
-        //$this->runCommands(['php artisan install:module-api']);
+        $this->runCommands(['php artisan install:api']);
 
         $files = new Filesystem;
 
