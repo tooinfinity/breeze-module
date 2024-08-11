e6dc56657f4c17356e4e4cd1e4eb531577fc11e4
TouwfiQ Meghlaoui <touwfiqdev@gmail.com>
2024-08-11 21:23:41 +0100

    fix typo module blade

--- src/Console/InstallsModuleBladeStack.php
+++ src/Console/InstallsModuleBladeStack.php
@@ -70,7 +70,7 @@ trait InstallsModuleBladeStack
         copy(__DIR__.'/../../stubs/module-api/database/seeders/DatabaseSeeder.php', base_path('Modules/Auth/database/seeders/DatabaseSeeder.php'));
 
         // Models
-        (new Filesystem)(__DIR__.'/../../stubs/module-api/app/Models', base_path('Modules/Auth/app/Models'));
+        (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/module-api/app/Models', base_path('Modules/Auth/app/Models'));
 
         // "Dashboard" Route...
         $this->replaceInFile('/home', '/dashboard', base_path('Modules/Auth/resources/views/welcome.blade.php'));
