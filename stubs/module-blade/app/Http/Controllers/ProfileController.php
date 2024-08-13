5bcc3438712868db9f1998eacb493a9c637e4132
TouwfiQ Meghlaoui <touwfiqdev@gmail.com>
2024-08-13 20:35:21 +0100

    fix

--- stubs/module-blade/app/Http/Controllers/ProfileController.php
+++ stubs/module-blade/app/Http/Controllers/ProfileController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use App\Http\Controllers\Controller;
 use Illuminate\View\View;
 
 class ProfileController extends Controller
